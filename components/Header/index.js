3f2a32a1ea731fab7f58bbc039a6cad9f616eebc
Jesus Gil <jesusgil.dev@gmail.com>
2022-04-24 21:35:59 -0500

    ignore:02

--- components/Header/index.js
+++ components/Header/index.js
@@ -44,35 +44,7 @@ export default function Header() {
   const linesProps = useSpring({ opacity, config: properties.springConfig });
 
   return (
-    <header
-      className={cn(
-        styles.header,
-        router.asPath !== '/' ? styles.headerNotHomePage : styles.headerHomePage
-      )}
-    >
-      {/*{router.asPath !== '/' ? (
-        <Link href="/">
-          <a className={styles.allPostsLink}>
-            <svg
-              width={24}
-              height={24}
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M7 16l-4-4m0 0l4-4m-4 4h18"
-              />
-            </svg>
-            <span>All Posts</span>
-          </a>
-        </Link>
-      ) : null}
-     */}
+    <header className={cn(styles.header)}>
       {hasMounted ? (
         <button
           className={styles.themeButton}

c5fef64ac0ac037124e6c111ac30b2e7624c259d
Jesus Gil <jesusgil.dev@gmail.com>
2022-04-26 09:49:04 -0500

    remove commented out react-reduce-motion hook setup in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,7 @@
-// import * as React from 'react';
 import { ThemeProvider } from 'next-themes';
-// import { useReduceMotion } from 'react-reduce-motion';
-// import { Globals } from 'react-spring';
 import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  // Currently doesn't work with server rendering:
-  // ReferenceError: window is not defined
-  // react: v18.0.0
-  // react-reduce-motion: v2.0.2
-
-  // const prefersReducedMotion = useReduceMotion();
-
-  // React.useEffect(() => {
-  //   Globals.assign({
-  //     skipAnimation: prefersReducedMotion,
-  //   });
-  // }, [prefersReducedMotion]);
-
   return (
     <ThemeProvider>
       <Component {...pageProps} />
